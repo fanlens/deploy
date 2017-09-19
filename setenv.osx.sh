bd26956648bcd65cb058e01082f85e23c6bb4758
Christian Junker <chjdev@gmail.com>
2017-09-19 10:26:10 +0200

    fix osx env setter

--- setenv.osx.sh
+++ setenv.osx.sh
@@ -1,9 +1,8 @@
-export (cat docker/config/eev.dev.env)
-export (cat docker/config/web.dev.env)
-export (cat docker/config/fanlens.dev.env)
-export (cat docker/config/worker.dev.env)
-export (cat docker/config/crawler.dev.env)
+export (cat config/bot.dev.env)
+export (cat config/web.dev.env)
+export (cat config/fanlens.dev.env)
+export (cat config/worker.dev.env)
+export (cat config/crawler.dev.env)
 set -gx FL_CRAWLER_HOST https://localhost
 # set -gx REQUESTS_CA_BUNDLE (pwd)/localhost.pem
-set PATH /opt/intel/intelpython35/bin/ $PATH
 set -gx GOOGLE_APPLICATION_CREDENTIALS (pwd)/gce/key.json
