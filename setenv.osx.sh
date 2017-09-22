85d0895f93565339e5a18aceca5a22f937fc075a
Christian Junker <chjdev@gmail.com>
2017-09-22 12:44:46 +0200

    move configs out of db into env

--- setenv.osx.sh
+++ setenv.osx.sh
@@ -1,6 +1,11 @@
 export (cat config/bot.dev.env)
+export (cat config/db.dev.env)
 export (cat config/web.dev.env)
-export (cat config/fanlens.dev.env)
+export (cat config/redis.dev.env)
+export (cat config/rabbitmq.dev.env)
+export (cat config/twitter.dev.env)
+export (cat config/celery.dev.env)
+export (cat config/mail.dev.env)
 export (cat config/worker.dev.env)
 export (cat config/crawler.dev.env)
 set -gx FL_CRAWLER_HOST https://localhost
