5661a2ea6bd397d8d3694ac3f1480ecbe9748526
Christian Junker <chjdev@gmail.com>
2017-09-25 09:42:47 +0200

    modified deploy for new config structure

--- bx/ui/deploy.sh
+++ bx/ui/deploy.sh
@@ -3,6 +3,8 @@ rm -rf common web
 gcp -al ../../common ./
 gcp -al ../../web ./
 cp manifest.yml.dist manifest.yml
-../setenv.bx.sh ui fanlens web ui >> manifest.yml
+../setenv.bx.sh version db redis celery mail web twitter >> manifest.yml
 bx cf push ui
 rm -rf common web
+
+
