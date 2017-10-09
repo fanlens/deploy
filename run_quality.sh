71cbba8d25daa28c4376ab44bf82f7208d27ea1d
Christian Junker <chjdev@gmail.com>
2017-10-09 15:41:19 +0200

    fix

--- run_quality.sh
+++ run_quality.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-#for module in common/common brain/brain crawler/crawler worker/worker web/web; do
-for module in web/web; do
+for module in common/common brain/brain crawler/crawler worker/worker web/web; do
   echo running quality checks for $module
   if [ -e $module/../pylintrc ]; then
-    echo $module/../pylintrc
+    echo special config at $module/../pylintrc
     pylint --rcfile=$module/../pylintrc $module
   else
     pylint $module
