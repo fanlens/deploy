620d2516aa60081b8ffb7bd4dd191faeb9955575
Christian Junker <chjdev@gmail.com>
2017-10-09 15:37:48 +0200

    quality sprint

--- run_quality.sh
+++ run_quality.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-for module in common/common brain/brain crawler/crawler worker/worker web/web; do
+#for module in common/common brain/brain crawler/crawler worker/worker web/web; do
+for module in web/web; do
   echo running quality checks for $module
-  pylint $module && mypy $module
+  if [ -e $module/../pylintrc ]; then
+    echo $module/../pylintrc
+    pylint --rcfile=$module/../pylintrc $module
+  else
+    pylint $module
+  fi
+  mypy $module
 done
